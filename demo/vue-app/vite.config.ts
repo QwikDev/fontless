22b12d47765ad70623ed4ae70e59eb6f996f3b5a
gioboa <giorgiob.boa@gmail.com>
2024-12-17 11:30:32 +0100

    chore: add Vue demo app

--- demo/vue-app/vite.config.ts
+++ demo/vue-app/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import vue from '@vitejs/plugin-vue'
 import { fontless } from "../../lib/vite/plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), fontless()],
+  plugins: [vue(), fontless()],
 })
