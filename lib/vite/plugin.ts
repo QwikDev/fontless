b5be3bb4d8d4eb8bc54dd2a0f446f83015138fec
gioboa <giorgiob.boa@gmail.com>
2024-12-17 11:27:31 +0100

    chore: add React demo app

--- lib/vite/plugin.ts
+++ lib/vite/plugin.ts
@@ -1,16 +1,17 @@
-import type { Plugin } from "vite";
-import { createUnifont, providers } from "unifont";
+import type { Plugin } from 'vite';
+import { createUnifont, providers } from 'unifont';
 
 export function fontless(): Plugin {
 	return {
-		name: "vite-plugin-fontless",
+		name: 'vite-plugin-fontless',
 
 		async configResolved() {
+			console.log('LOG: fontless - configResolved');
 			const unifont = await createUnifont([providers.google()]);
 		},
 
 		async transform(code: string, id: string) {
-			if (!id.endsWith(".css")) return;
+			if (!id.endsWith('.css')) return;
 		},
 	};
 }
