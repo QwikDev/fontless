82a1c0a8a988e1e713aa878fdafd6ab84afe93d4
jack shelton <me@jackshelton.com>
2024-12-17 23:23:28 -0600

    use ast on css files

--- lib/vite/plugin.ts
+++ lib/vite/plugin.ts
@@ -1,17 +1,22 @@
-import type { Plugin } from 'vite';
-import { createUnifont, providers } from 'unifont';
+import type { Plugin } from "vite";
+import { createUnifont, providers } from "unifont";
+import { parse, walk } from "css-tree";
 
 export function fontless(): Plugin {
 	return {
-		name: 'vite-plugin-fontless',
+		name: "vite-plugin-fontless",
 
 		async configResolved() {
-			console.log('LOG: fontless - configResolved');
+			console.log("LOG: fontless - configResolved");
 			const unifont = await createUnifont([providers.google()]);
 		},
 
 		async transform(code: string, id: string) {
-			if (!id.endsWith('.css')) return;
+			if (!id.includes(".css")) return;
+
+			const ast = parse(code, { positions: true });
+
+			console.log("ast: ", ast);
 		},
 	};
 }
