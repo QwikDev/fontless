abe02415fdab4a77288f5fec6d32650a1d1f5325
Damián Pumar <damianpumar@gmail.com>
2025-01-08 10:51:00 +0100

    feat: :zap: Vue app

--- demo/vue-app/src/App.vue
+++ demo/vue-app/src/App.vue
@@ -1,24 +1,59 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div>
-    <h1>Google provider</h1>
-    <div>
-      Poppins
-    </div>
-    <p>
-      Press Start 2P
-    </p>
+    <h1>Google</h1>
+    <p class="poppins">Poppins</p>
+    <p class="press-start">Press Start 2P</p>
+
+    <h1>Bunny</h1>
+    <p class="bunny-aclonica">Aclonica</p>
+    <p class="bunny-allan">Allan</p>
+
+    <h1>FontShare</h1>
+    <p class="font-share-panchang">Panchang</p>
+
+    <h1>FontSource</h1>
+    <p class="font-source-luckiest">Luckiest</p>
+
+    <h1>Local</h1>
+    <p class="local">Local font</p>
   </div>
 </template>
 
 <style scoped>
-div {
-  font: 1.2em 'Poppins', sans-serif;
+p {
+  font-size: x-large;
 }
 
-p {
-  font-family: 'Press Start 2P', sans-serif;
+.google-poppins {
+  font-family: "Poppins", sans-serif;
+}
+
+.google-press-start {
+  font-family: "Press Start 2P", sans-serif;
+}
+
+.bunny-aclonica {
+  font-family: "Aclonica", sans-serif;
+}
+
+.bunny-allan {
+  font-family: "Allan", sans-serif;
+}
+
+.font-share-panchang {
+  font-family: "Panchang", sans-serif;
+}
+
+.font-source-luckiest {
+  font-family: "Luckiest Guy", sans-serif;
+}
+
+@font-face {
+  font-family: "Black Fox";
+  src: url("../src/assets/fonts/black-fox.ttf");
+}
+
+.local {
+  font-family: "Black Fox", sans-serif;
 }
 </style>
